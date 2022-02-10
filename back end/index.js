15ec926e80d068d9feec2f929bc4f3109983fc24
Navaneeth V <navaneethnair69@gmail.com>
2022-02-11 01:47:19 +0530

    commit

--- back end/index.js
+++ back end/index.js
@@ -1,11 +1,19 @@
 const express = require('express'); //importing express
 const cors = require('cors');
 const ArticleInfo = require('./src/model/BlogDB'); // importing mongodb
+const path = require('path');
 
 const app = express(); // creating a new object for express as app //object initialization
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); //For POST Method
 app.use(express.json());
+app.use(express.static('./build/'));
+
+app.get(’/’, function(req, res) => {
+    res.sendFile(path.join( 
+   ./build/index.html')
+   });
+   
 
 //Basic article fetch route
 app.get('/api/article/:name', (req, res) => {// to fetch general data
