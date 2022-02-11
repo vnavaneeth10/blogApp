9cdb4ef9ecb7594e9226a6e0ddcd5d32aba2ee43
Navaneeth V <navaneethnair69@gmail.com>
2022-02-11 22:35:10 +0530

    commit

--- back end/src/model/BlogDB.js
+++ back end/src/model/BlogDB.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
- mongoose.connect('mongodb://localhost:27017/blog-app');
-//  mongoose.connect('mongodb+srv://navi:N1kobme0a4RSdnPW@cluster0.de7kc.mongodb.net/articles?retryWrites=true&w=majority');
+// mongoose.connect('mongodb://localhost:27017/blog-app');
+mongoose.connect('mongodb+srv://navi:N1kobme0a4RSdnPW@cluster0.de7kc.mongodb.net/articles?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
-var articleSchema = new Schema({ //Schema is used to structurize the DB
+var articleSchema = new Schema({  //Schema is used to structurize the DB
+    
     name: String,
     username: String,
     upvotes: Number,
     comments: Array
+
 });
 
 var ArticleInfo = mongoose.model('articles', articleSchema); //Mentioned articles collection name in to the schema
